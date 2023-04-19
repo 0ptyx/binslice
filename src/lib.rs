f52da4f5c276ecb1d72b4f09204d32738144b251
David Golembiowski <david.golembiowski@tangramflex.com>
2023-04-19 17:48:37 -0400

    Adding debug representations for BinSlice types

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,60 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod binslice {
+    use std::fmt::{self, Display, Formatter};
+    use std::io;
+    use std::path::{self, Path};
+    use std::rc::Rc;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    // Want to devise a trait for T to constrain
+    // what it could be -- something meaningful to
+    // being able to spit back the cleaned up binary
+    // data, reach into its fields, and potentially
+    // re-store it as a C-FFI binary data dump
+    struct BinSlice<T>
+    where
+        T: Entry + Display + Sized,
+    {
+        data: Box<Vec<u8>>,
+        end: Endianness,
+        info: Option<Details<T>>,
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    enum Endianness {
+        Big,
+        Little,
+        Mixed,
+        Unknown,
+    }
+    struct Details<E: Entry + Display + Sized> {
+        title: String,
+        entries: Box<Vec<Rc<E>>>,
+    }
+
+    impl<E> Details<E>
+    where
+        E: Entry + Display + Sized,
+    {
+        fn print(&self, dest: impl AsRef<Path>) -> io::Result<()> {
+            todo!("Need to flesh this out");
+        }
+
+        // Needs to acquire exclusive write access to STDERR
+        fn to_stderr(&self) -> io::Result<()> {
+            todo!("Need to flesh this out");
+        }
+    }
+
+    trait Entry {}
+
+    impl<E> fmt::Display for Details<E>
+    where
+        E: Entry + Display + Sized,
+    {
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+            write!(f, "{}\n", self.title);
+            for entry in self.entries.clone().into_iter() {
+                write!(f, "{}", &entry);
+            }
+            fmt::Result::Ok(())
+        }
     }
 }
